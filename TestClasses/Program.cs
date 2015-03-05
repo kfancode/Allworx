89516afd8b729b3381c5d30a480ed5674afe3556
kfancode <kfanion@gmail.com>
2015-03-05 08:37:03 -0500

    stable

--- TestClasses/Program.cs
+++ TestClasses/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AllworxLib;
+using System.Data.Linq;
 
 namespace TestClasses
 {
@@ -15,7 +16,24 @@ namespace TestClasses
             Console.WriteLine(c.GetCall());
         
             Console.WriteLine("From:  " + c.GetFrom() + "\nTo:  " + c.GetTo());
+            
+
+            // DataContext takes a connection string. 
+            AllworxDataContext db = new AllworxDataContext();
+
+            // Get a typed table to run queries.
+            Table<chargedcall> chCalls = db.GetTable<chargedcall>();
+
+            // Query for customers from London. 
+            var query =
+                from call in chCalls
+                select call;
+
+            foreach (var call in query)
+                Console.WriteLine("id = {0}, City = {1}", call.placedto, call.placedfrom);
+
             Console.ReadLine();
+                 
         }
     }
 }
